d8a0380251d53c47ac0cae54a9af5522e03d0742
Kevin Charter <kevin.charter@acm.org>
2011-08-20 19:28:29 -0600

    Make the 'typeId' and 'idNum' fields strict in Erl.Entity.Id.
    Probably a premature performance optimisation, but I suspect it's a good idea.

--- Erl/Entity.hs
+++ Erl/Entity.hs
@@ -4,5 +4,5 @@ import qualified Erl.EntityType as Type
 
 newtype IdNum = IdNum Int deriving (Eq, Ord, Show)
 
-data Id = Unscoped { typeId :: Type.Id, idNum :: IdNum } |
-          Scoped   { typeId :: Type.Id, idNum :: IdNum, ownerId :: Id } deriving (Eq, Ord, Show)
+data Id = Unscoped { typeId :: !Type.Id, idNum :: !IdNum } |
+          Scoped   { typeId :: !Type.Id, idNum :: !IdNum, ownerId :: Id } deriving (Eq, Ord, Show)
