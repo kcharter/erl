e9ecb59a94057d3148dfd2cd297c02c513f1a2d6
Kevin Charter <kevin.charter@acm.org>
2011-08-20 19:37:28 -0600

    Add entity names and instances.
    We use Text for names, and allow arbitrary data in the 'attributes'
    of an instance.
    
    I've deliberately made the type of attributes opaque in order to
    simplify the interface.

--- Erl/Entity.hs
+++ Erl/Entity.hs
@@ -1,8 +1,14 @@
 module Erl.Entity where
 
+import qualified Data.Text as T
+
 import qualified Erl.EntityType as Type
 
 newtype IdNum = IdNum Int deriving (Eq, Ord, Show)
 
 data Id = Unscoped { typeId :: !Type.Id, idNum :: !IdNum } |
           Scoped   { typeId :: !Type.Id, idNum :: !IdNum, ownerId :: Id } deriving (Eq, Ord, Show)
+
+newtype Name = Name T.Text deriving (Eq, Ord, Show)
+
+data Instance d = Instance { id :: Id, name :: Name, attributes :: d }
