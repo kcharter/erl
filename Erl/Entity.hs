08db36bbf54761edd462c83b750bd68d259b6f52
Kevin Charter <kevin.charter@acm.org>
2011-08-26 23:03:23 -0600

    Make Erl.Entity.IdNum an instance of Enum.
    We'll want to be able to call 'succ' in order to generate
    fresh ID numbers for new instances.

--- Erl/Entity.hs
+++ Erl/Entity.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Erl.Entity where
 
 import qualified Erl.EntityType as Type
 
-newtype IdNum = IdNum Int deriving (Eq, Ord, Show)
+newtype IdNum = IdNum Int deriving (Eq, Ord, Enum, Show)
 
 data Id = Unscoped { typeId :: !Type.Id, idNum :: !IdNum } |
           Scoped   { typeId :: !Type.Id, idNum :: !IdNum, ownerId :: Id } deriving (Eq, Ord, Show)
