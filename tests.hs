2f9dc9c34dc2952cdf78d996c0ee23e19a60a46a
Kevin Charter <kevin.charter@acm.org>
2011-08-26 21:24:45 -0600

    Add unit tests for the entity type method implementations.
    The 'tests.hs' program contains the first batch of QuickCheck unit
    tests. I've added three separate tests for the effects of adding an
    entity type. The 'Name' and 'ErlTState' types have Arbitrary
    instances; currently an arbitrary ErlTState is just the result of
    adding a generated list of entity types to the empty state.
    
    As of version 1.9.2, Cabal supports building and running unit test
    suites. I've added a test suite configuration to the cabal file, and
    bumped up the minimum cabal version.
    
    As far as I can tell, the simplest way to integrate QuickCheck unit
    tests is to use the 'exitcode-stdio-1.0' test type, where we have to
    create a test executable that returns a non-zero exit code if any of
    the tests fails.  In order to do this, we need to use the
    'quickCheckResult' function for each test, and 'isSuccess' to check
    that all the results are successful; the ordinary 'checkCheck'
    function only prints test results to standard output, and if we used
    it we couldn't return a non-zero exit code on failure.
    
    Cabal also supports a test suite type called 'detailed-1.0', which
    allows Cabal to interact with the test suite and do its own detailed
    logging. I don't think the lastest version of QuickCheck has
    simplified support for this interface yet. I looked quickly and
    couldn't find any specific cabal support, so I stuck with
    'exitcode-stdio-1.0'.
    
    Unfortunately, we end up having to repeat the dependencies of the main
    project in the 'build-depends' part of the test suite
    configuration. I'll check the Cabal documentation to see whether there
    is a way to avoid this, but I'm not sure that there is.

---
+++ tests.hs
@@ -0,0 +1,55 @@
+module Main where
+
+import Control.Monad (unless, liftM)
+import Control.Monad.Error (catchError)
+import Data.String
+import System.Exit (exitFailure)
+import Test.QuickCheck
+import Test.QuickCheck.Test (isSuccess)
+
+import qualified Erl.EntityType as ET
+import Erl.Monad
+import Erl.Name
+
+main :: IO ()
+main = do
+  rs <- sequence [quickCheckResult prop_createEntityType1,
+                  quickCheckResult prop_createEntityType2,
+                  quickCheckResult prop_createEntityType3]
+  unless (all isSuccess rs) exitFailure
+
+prop_createEntityType1 :: (ErlTState, Name) -> Bool
+prop_createEntityType1 (s, name) = prop_createEntityTypeGeneral canGetIdByName (s, name)
+  where canGetIdByName newId = maybe False (newId ==) `liftM` lookupEntityTypeName name
+
+prop_createEntityType2 :: (ErlTState, Name) -> Bool
+prop_createEntityType2 (s, name) = prop_createEntityTypeGeneral idIsInAllIds (s, name)
+  where idIsInAllIds newId = elem newId `liftM` entityTypeIds
+
+prop_createEntityType3 :: (ErlTState, Name) -> Bool
+prop_createEntityType3 (s, name) = prop_createEntityTypeGeneral canGetEntityType (s, name)
+  where canGetEntityType etId = testEntityType `liftM` entityType etId
+          where testEntityType t = ET.id t == etId && ET.name t == name
+
+prop_createEntityTypeGeneral :: (ET.Id -> ErlMonad () Bool) -> (ErlTState, Name) -> Bool
+prop_createEntityTypeGeneral whenNew (s, name)  =
+  checkErl s $
+    maybe createAndTest checkDuplicateError =<< lookupEntityTypeName name
+      where createAndTest = whenNew =<< createEntityType name
+            checkDuplicateError _ =
+              (createEntityType name >> return False) `catchError` (const $ return True)
+
+checkErl :: ErlTState -> ErlMonad () Bool -> Bool
+checkErl s erl =
+  either (const False) id $ evalErl erl s
+
+instance Arbitrary Name where
+  arbitrary = do
+    base <- elements $ map (:[]) ['A'..'Z']
+    suffix <- frequency [(10, return Nothing), (90, Just `liftM` elements [1..100])]
+    return $ fromString $ maybe base ((base ++) . show) suffix
+
+instance Arbitrary ErlTState where
+  arbitrary = foldr addEntityType emptyState `liftM` arbitrary
+    where addEntityType name =
+            execErl (createEntityType name)
