5b10a69d466e19f0f5d45c7ff3b90225dc774222
Kevin Charter <kevin.charter@acm.org>
2011-08-26 18:11:55 -0600

    Make Erl.Name.Name an instance of IsString.
    This will make it more convenient to use, especially for
    interactive tests in ghci.

--- Erl/Name.hs
+++ Erl/Name.hs
@@ -1,5 +1,9 @@
 module Erl.Name where
 
+import Data.String
 import qualified Data.Text as T
 
 newtype Name = Name T.Text deriving (Eq, Ord, Show)
+
+instance IsString Name where
+  fromString = Name . T.pack
