77e87129d97f41660c595311d64c9f6e387a674a
Kevin Charter <kevin.charter@acm.org>
2011-08-26 21:13:29 -0600

    Implement the entity type methods for ErlMonad(T).
    This implements the MonadErl type class methods
    
     createEntityType
     entityTypeIds
     lookupEntityTypeName
     lookupEntityType
    
    for ErlT and ErlMonad. In the process, I've made some changes
    and additions in Erl.Monad.hs:
    
    1. the MonadErl type class method
    
         entityType :: Id -> m EntityType
    
       is now a top-level function
    
         entityType :: (MonadErl d m) => Id -> m EntityType
    
       and is replaced in MonadErl by two lower-level methods
    
         lookupEntityType :: Id -> m (Maybe EntityType)
         lookupEntityTypeName :: Name -> m (Maybe Id)
    
    2. I've also added
    
         hasEntityTypeNamed :: (MonadErl d m) => Name -> m Bool
    
    3. ErlT derives MonadState ErlTState in order to make it trivial
       for MonadErl to derive MonadState ErlTState
    
    4. ErlTState now has sufficient fields to implement the
       entity type methods of MonadErl for ErlT
    
    5. I've added a Show instance for ErlTState in order to support
       unit tests. The 'show' implementation doesn't show all the
       details; that may be a mistake.
    
    6. I've added the functions
    
         doErlT
         evalErlT
         execErlT
    
       for ErlT and the corresponding functions
    
         doErl
         evalErl
         execErl
    
       for ErlMonad.
    
    I'll add the first batch of tests in the next commit.

--- Erl/Monad.hs
+++ Erl/Monad.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
 
-module Erl.Monad where
+module Erl.Monad (ErlError(..),
+                  MonadErl(..),
+                  hasEntityTypeNamed,
+                  entityType,
+                  ErlT(..),
+                  ErlTState,
+                  emptyState,
+                  doErlT,
+                  evalErlT,
+                  execErlT,
+                  ErlMonad(..),
+                  doErl,
+                  evalErl,
+                  execErl) where
 
 import Control.Monad.Error
 import Control.Monad.State
 import Control.Monad.Identity
+import qualified Data.Map as DM
 
 import Erl.Name
 import qualified Erl.EntityType as ET
@@ -18,31 +33,65 @@ data ErlError = ErlError String deriving (Eq, Ord, Show)
 instance Error ErlError where
   strMsg = ErlError
 
-class (MonadError ErlError m) => MonadErl d m where
+class (MonadError ErlError m) => MonadErl d m | m -> d where
   createEntityType :: Name -> m ET.Id
   entityTypeIds :: m [ET.Id]
-  entityType :: ET.Id -> m ET.EntityType
+  lookupEntityType :: ET.Id -> m (Maybe ET.EntityType)
+  lookupEntityTypeName :: Name -> m (Maybe ET.Id)
   entityIds :: ET.Id -> m [E.Id]
   entity :: E.Id -> m (E.Instance d)
   createEntity :: ET.Id -> Name -> d -> m E.Id
   deleteEntity :: E.Id -> m ()
   updateEntity :: E.Id -> (d -> d) -> m ()
   renameEntity :: E.Id -> Name -> m ()
-  
+
+hasEntityTypeNamed :: (MonadErl d m) => Name -> m Bool
+hasEntityTypeNamed name = maybe False (const True) `liftM` lookupEntityTypeName name
+
+entityType :: (MonadErl d m) => ET.Id -> m ET.EntityType
+entityType id = maybe noSuchType return =<< lookupEntityType id
+  where noSuchType = throwMsg $ "There is no entity type with id " ++ show id ++ "."
+
 newtype ErlT d m a =
   ErlT { runErlT :: ErrorT ErlError (StateT ErlTState m) a }
-  deriving (Monad, (MonadError ErlError), MonadIO)
+  deriving (Monad, MonadError ErlError, MonadState ErlTState, MonadIO)
 
-data ErlTState = ErlTState
+doErlT :: (Monad m) => ErlT d m a -> ErlTState -> m (Either ErlError a, ErlTState)
+doErlT erl state = runStateT (runErrorT $ runErlT erl) state
+
+evalErlT :: (Monad m) => ErlT d m a -> ErlTState -> m (Either ErlError a)
+evalErlT erl state = evalStateT (runErrorT $ runErlT erl) state
+
+execErlT :: (Monad m) => ErlT d m a -> ErlTState -> m ErlTState
+execErlT erl state = execStateT (runErrorT $ runErlT erl) state
 
 newtype ErlMonad d a =
   ErlMonad { runErl ::  ErlT d Identity a }
-  deriving (Monad, MonadError ErlError)
+  deriving (Monad, MonadError ErlError, MonadErl d)
+
+doErl :: ErlMonad d a -> ErlTState -> (Either ErlError a, ErlTState)
+doErl erl state = runIdentity $ doErlT (runErl erl) state
+
+evalErl :: ErlMonad d a -> ErlTState -> Either ErlError a
+evalErl erl state = runIdentity $ evalErlT (runErl erl) state
+
+execErl :: ErlMonad d a -> ErlTState -> ErlTState
+execErl erl state = runIdentity $ execErlT (runErl erl) state
 
 instance (Monad m) => MonadErl d (ErlT d m) where
-  createEntityType = ni
-  entityTypeIds = ni
-  entityType = ni
+  createEntityType name = do
+    s <- get
+    maybe (makeNew s) alreadyExists $ DM.lookup name (typeIdsByName s)
+      where alreadyExists = const $ throwMsg $ "There is already an entity type " ++ show name ++ "."
+            makeNew s = put s' >> return etId
+              where etId = nextEntityTypeId s
+                    s' = s { nextEntityTypeId = succ etId,
+                             typesById = DM.insert etId et (typesById s),
+                             typeIdsByName = DM.insert name etId (typeIdsByName s) }
+                    et = ET.EntityType { ET.id = etId, ET.name = name }
+  entityTypeIds = (DM.keys . typesById) `liftM` get
+  lookupEntityTypeName name = (DM.lookup name . typeIdsByName) `liftM` get
+  lookupEntityType id = (DM.lookup id . typesById) `liftM` get
   entityIds = ni
   entity = ni
   createEntity = ni
@@ -50,4 +99,23 @@ instance (Monad m) => MonadErl d (ErlT d m) where
   updateEntity = ni
   renameEntity = ni
 
+data ErlTState = ErlTState {
+  nextEntityTypeId :: ET.Id,
+  typesById :: DM.Map ET.Id ET.EntityType,
+  typeIdsByName :: DM.Map Name ET.Id
+  }
+
+emptyState :: ErlTState
+emptyState = ErlTState {
+  nextEntityTypeId = ET.Id 0,
+  typesById = DM.empty,
+  typeIdsByName = DM.empty
+  }
+
+throwMsg :: (Error e, MonadError e m) => String -> m a
+throwMsg = throwError . strMsg
+
 ni = error "Not implemented"
+
+instance Show ErlTState where
+  show s = "ErlTState { entityTypes = " ++ show (map ET.name $ DM.elems $ typesById s) ++ " }"
