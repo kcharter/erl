bee581e1e57f3e2e9aa2294f2d813d03e6da0280
Kevin Charter <kevin.charter@acm.org>
2011-09-01 16:03:00 -0600

    Remove the Erl.EntityType module.

--- Erl/EntityType.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
-module Erl.EntityType where
-
-import Erl.Name
-
-newtype Id = Id Int deriving (Eq, Ord, Enum, Show)
-
-data EntityType = EntityType { id :: Id, name :: Name } deriving (Eq, Ord, Show)
