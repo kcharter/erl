a2c450032b6238ac8011884278c78363b38985e5
Kevin Charter <kevin.charter@acm.org>
2011-09-01 14:51:39 -0600

    Stop using entity type IDs in Erl.Entity.EntityType.
    None of the code in Erl.Monad creates entity IDs, so this change
    doesn't break anything.

--- Erl/Entity.hs
+++ Erl/Entity.hs
@@ -2,10 +2,8 @@
 
 module Erl.Entity where
 
-import qualified Erl.EntityType as Type
-
 newtype IdNum = IdNum Int deriving (Eq, Ord, Enum, Show)
 
-data EntityId = EntityId { typeId :: !Type.Id, idNum :: !IdNum } deriving (Eq, Ord, Show)
+data EntityId = EntityId { idNum :: !IdNum } deriving (Eq, Ord, Show)
 
 data Entity d = Entity { id :: EntityId, attributes :: d } deriving (Eq, Ord, Show)
