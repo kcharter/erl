b8d57ec2bae2474c7560c9e4e10a1f592b8583ae
Kevin Charter <kevin.charter@acm.org>
2011-09-01 14:42:53 -0600

    Rename the EntityId constructor to 'EntityId'.
    We don't create entity instances yet, so this doesn't require changes
    in other code.

--- Erl/Entity.hs
+++ Erl/Entity.hs
@@ -6,6 +6,6 @@ import qualified Erl.EntityType as Type
 
 newtype IdNum = IdNum Int deriving (Eq, Ord, Enum, Show)
 
-data EntityId = Unscoped { typeId :: !Type.Id, idNum :: !IdNum } deriving (Eq, Ord, Show)
+data EntityId = EntityId { typeId :: !Type.Id, idNum :: !IdNum } deriving (Eq, Ord, Show)
 
 data Instance d = Instance { id :: EntityId, attributes :: d } deriving (Eq, Ord, Show)
