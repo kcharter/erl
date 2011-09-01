6d203905730e63364e36509115ecdc9a010afc64
Kevin Charter <kevin.charter@acm.org>
2011-09-01 14:53:38 -0600

    Remove Erl.Entity.IdNum, and simplify Erl.Entity.EntityId.
    This 'inlines' IdNum into EntityId. This doesn't break any other code.

--- Erl/Entity.hs
+++ Erl/Entity.hs
@@ -2,8 +2,6 @@
 
 module Erl.Entity where
 
-newtype IdNum = IdNum Int deriving (Eq, Ord, Enum, Show)
-
-data EntityId = EntityId { idNum :: !IdNum } deriving (Eq, Ord, Show)
+newtype EntityId = EntityId Int deriving (Eq, Ord, Enum, Show)
 
 data Entity d = Entity { id :: EntityId, attributes :: d } deriving (Eq, Ord, Show)
