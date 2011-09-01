86eea492ce872c770a68158ab27dc1202788a87a
Kevin Charter <kevin.charter@acm.org>
2011-09-01 16:02:38 -0600

    Remove all the uses of entity types (and the Erl.Name module).
    This deletes most of what I had written as of yesterday.
    
    I've left the Erl.EntityType and Erl.Name modules for the moment;
    I'll delete them in the next commit.

--- Erl/Monad.hs
+++ Erl/Monad.hs
@@ -6,8 +6,6 @@
 
 module Erl.Monad (ErlError(..),
                   MonadErl(..),
-                  hasEntityTypeNamed,
-                  entityType,
                   ErlT(..),
                   ErlTState,
                   emptyState,
@@ -24,8 +22,6 @@ import Control.Monad.State
 import Control.Monad.Identity
 import qualified Data.Map as DM
 
-import Erl.Name
-import qualified Erl.EntityType as ET
 import qualified Erl.Entity as E
 
 data ErlError = ErlError String deriving (Eq, Ord, Show)
@@ -34,22 +30,11 @@ instance Error ErlError where
   strMsg = ErlError
 
 class (MonadError ErlError m) => MonadErl d m | m -> d where
-  createEntityType :: Name -> m ET.Id
-  entityTypeIds :: m [ET.Id]
-  lookupEntityType :: ET.Id -> m (Maybe ET.EntityType)
-  lookupEntityTypeName :: Name -> m (Maybe ET.Id)
   selectEntities :: (d -> Bool) -> m [E.EntityId]
   lookupEntity :: E.EntityId -> m (Maybe (E.Entity d))
   createEntity :: d -> m E.EntityId
   deleteEntity :: E.EntityId -> m ()
   updateEntity :: E.EntityId -> (d -> d) -> m ()
-
-hasEntityTypeNamed :: (MonadErl d m) => Name -> m Bool
-hasEntityTypeNamed name = maybe False (const True) `liftM` lookupEntityTypeName name
-
-entityType :: (MonadErl d m) => ET.Id -> m ET.EntityType
-entityType id = maybe noSuchType return =<< lookupEntityType id
-  where noSuchType = throwMsg $ "There is no entity type with ID " ++ show id ++ "."
 
 entity :: (MonadErl d m) => E.EntityId -> m (E.Entity d)
 entity id = maybe noSuchInstance return =<< lookupEntity id
@@ -82,20 +67,6 @@ execErl :: ErlMonad d a -> ErlTState d -> ErlTState d
 execErl erl state = runIdentity $ execErlT (runErl erl) state
 
 instance (Monad m) => MonadErl d (ErlT d m) where
-  createEntityType name = do
-    s <- get
-    maybe (makeNew s) alreadyExists $ DM.lookup name (typeIdsByName s)
-      where alreadyExists = const $ throwMsg $ "There is already an entity type " ++ show name ++ "."
-            makeNew s = put s' >> return etId
-              where etId = nextEntityTypeId s
-                    s' = s { nextEntityTypeId = succ etId,
-                             entityTypeRecsById = DM.insert etId etr (entityTypeRecsById s),
-                             typeIdsByName = DM.insert name etId (typeIdsByName s) }
-                    etr = newEntityTypeRec et
-                    et = ET.EntityType { ET.id = etId, ET.name = name }
-  entityTypeIds = (DM.keys . entityTypeRecsById) `liftM` get
-  lookupEntityTypeName name = (DM.lookup name . typeIdsByName) `liftM` get
-  lookupEntityType id = (fmap theType . DM.lookup id . entityTypeRecsById) `liftM` get
   selectEntities pred = (map E.id . DM.elems . DM.filter pred' . allEntities) `liftM` get
     where pred' = pred . E.attributes
   lookupEntity id = (DM.lookup id . allEntities) `liftM` get
@@ -112,31 +83,14 @@ instance (Monad m) => MonadErl d (ErlT d m) where
   updateEntity = ni
 
 data ErlTState d = ErlTState {
-  nextEntityTypeId :: ET.Id,
-  entityTypeRecsById :: DM.Map ET.Id (EntityTypeRec d),
-  typeIdsByName :: DM.Map Name ET.Id,
   nextEntityId :: E.EntityId,
   allEntities :: DM.Map E.EntityId (E.Entity d)
-  }
-
-data EntityTypeRec d = EntityTypeRec {
-  theType :: ET.EntityType,
-  instancesById :: DM.Map E.EntityId (E.Entity d)
-  }
+  } deriving (Show)
 
 emptyState :: ErlTState d
 emptyState = ErlTState {
-  nextEntityTypeId = ET.Id 0,
-  entityTypeRecsById = DM.empty,
-  typeIdsByName = DM.empty,
   nextEntityId = E.EntityId 0,
   allEntities = DM.empty
-  }
-
-newEntityTypeRec :: ET.EntityType -> EntityTypeRec d
-newEntityTypeRec t = EntityTypeRec {
-  theType = t,
-  instancesById = DM.empty
   }
 
 throwMsg :: (Error e, MonadError e m) => String -> m a
@@ -144,6 +98,3 @@ throwMsg = throwError . strMsg
 
 ni = error "Not implemented"
 
-instance Show (ErlTState d) where
-  show s = "ErlTState { entityTypes = " ++
-           show (map (ET.name . theType) $ DM.elems $ entityTypeRecsById s) ++ " }"
--- tests.hs
+++ tests.hs
@@ -7,42 +7,17 @@ import System.Exit (exitFailure)
 import Test.QuickCheck
 import Test.QuickCheck.Test (isSuccess)
 
-import qualified Erl.EntityType as ET
 import qualified Erl.Entity as E
 import Erl.Monad
 import Erl.Name
 
 main :: IO ()
 main = do
-  rs <- sequence [quickCheckResult prop_createEntityType1,
-                  quickCheckResult prop_createEntityType2,
-                  quickCheckResult prop_createEntityType3,
-                  quickCheckResult prop_createEntity1,
+  rs <- sequence [quickCheckResult prop_createEntity1,
                   quickCheckResult prop_createEntity2,
                   quickCheckResult prop_deleteEntity1,
                   quickCheckResult prop_deleteEntity2]
   unless (all isSuccess rs) exitFailure
-
-prop_createEntityType1 :: (ErlTState (), Name) -> Bool
-prop_createEntityType1 (s, name) = prop_createEntityTypeGeneral canGetIdByName (s, name)
-  where canGetIdByName newId = maybe False (newId ==) `liftM` lookupEntityTypeName name
-
-prop_createEntityType2 :: (ErlTState (), Name) -> Bool
-prop_createEntityType2 (s, name) = prop_createEntityTypeGeneral idIsInAllIds (s, name)
-  where idIsInAllIds newId = elem newId `liftM` entityTypeIds
-
-prop_createEntityType3 :: (ErlTState (), Name) -> Bool
-prop_createEntityType3 (s, name) = prop_createEntityTypeGeneral canGetEntityType (s, name)
-  where canGetEntityType etId = testEntityType `liftM` entityType etId
-          where testEntityType t = ET.id t == etId && ET.name t == name
-
-prop_createEntityTypeGeneral :: (ET.Id -> ErlMonad () Bool) -> (ErlTState (), Name) -> Bool
-prop_createEntityTypeGeneral whenNew (s, name)  =
-  checkErl s $
-    maybe createAndTest checkDuplicateError =<< lookupEntityTypeName name
-      where createAndTest = whenNew =<< createEntityType name
-            checkDuplicateError _ =
-              (createEntityType name >> return False) `catchError` (const $ return True)
 
 prop_createEntity1 :: (ErlTState Int, Int) -> Bool
 prop_createEntity1 (s, val) =
@@ -73,19 +48,9 @@ checkErl :: ErlTState d -> ErlMonad d Bool -> Bool
 checkErl s erl =
   either (const False) id $ evalErl erl s
 
-instance Arbitrary Name where
-  arbitrary = do
-    base <- elements $ map (:[]) ['A'..'Z']
-    suffix <- frequency [(10, return Nothing), (90, Just `liftM` elements [1..100])]
-    return $ fromString $ maybe base ((base ++) . show) suffix
-
 instance (Arbitrary d) => Arbitrary (ErlTState d) where
-  arbitrary = do
-    s1 <- foldr addEntityType emptyState `liftM` arbitrary
-    s2 <- foldr addEntity s1 `liftM` arbitrary
-    return s2
-    where addEntityType = execErl . createEntityType
-          addEntity = execErl . createEntity
+  arbitrary = foldr addEntity emptyState `liftM` arbitrary
+    where addEntity = execErl . createEntity
 
 instance Arbitrary E.EntityId where
   arbitrary = E.EntityId `liftM` arbitrary
