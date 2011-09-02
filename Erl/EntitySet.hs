5d59e56f0c5f0bdaf9c137e8be5688e507ebeca9
Kevin Charter <kevin.charter@acm.org>
2011-09-01 20:12:15 -0600

    Add the Erl.EntitySet module.
    Erl.EntitySet is just a newtype on Data.IntSet with automatic
    conversion between integers and entity IDs. It also helps
    limit the interface to just the minimum necessary operations,
    and insulates the rest of the code from depending directly on
    Data.IntSet.
    
    Ultimately, we'll want to be able to efficiently compute unions,
    differences, and intersections of sets of entity IDs. In the
    next commit, I'll replace lists of EntityId with EntitySet in
    the MonadErl methods.

---
+++ Erl/EntitySet.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Erl.EntitySet (EntitySet,
+                      empty,
+                      singleton,
+                      add,
+                      remove,
+                      contains,
+                      union,
+                      difference,
+                      intersection,
+                      toList,
+                      fromList) where
+
+import qualified Data.IntSet as DIS
+
+import Erl.Entity
+
+newtype EntitySet = EntitySet DIS.IntSet deriving (Eq, Ord, Show)
+
+empty :: EntitySet
+empty = EntitySet DIS.empty
+
+singleton :: EntityId -> EntitySet
+singleton = fromIntSet . DIS.singleton . idToInt
+
+add :: EntityId -> EntitySet -> EntitySet
+add id es = fromIntSet $ DIS.insert (idToInt id) (toIntSet es)
+
+remove :: EntityId -> EntitySet -> EntitySet
+remove id es = fromIntSet $ DIS.delete (idToInt id) (toIntSet es)
+
+contains :: EntitySet -> EntityId -> Bool
+contains es id = DIS.member (idToInt id) (toIntSet es)
+
+union :: EntitySet -> EntitySet -> EntitySet
+union = lift2 DIS.union
+
+difference :: EntitySet -> EntitySet -> EntitySet
+difference = lift2 DIS.difference
+
+intersection :: EntitySet -> EntitySet -> EntitySet
+intersection = lift2 DIS.intersection
+
+lift2 :: (DIS.IntSet -> DIS.IntSet -> DIS.IntSet) -> EntitySet -> EntitySet -> EntitySet
+lift2 f es1 es2 = fromIntSet $ f (toIntSet es1) (toIntSet es2)
+
+toList :: EntitySet -> [EntityId]
+toList es = map intToId $ DIS.toList $ toIntSet es
+
+fromList :: [EntityId] -> EntitySet
+fromList = fromIntSet . DIS.fromList . map idToInt
+
+idToInt :: EntityId -> Int
+idToInt (EntityId i) = i
+
+intToId :: Int -> EntityId
+intToId = EntityId
+
+toIntSet :: EntitySet -> DIS.IntSet
+toIntSet (EntitySet s) = s
+
+fromIntSet :: DIS.IntSet -> EntitySet
+fromIntSet = EntitySet
