dee124d6268c1cbc4f939ee7bc21e4097805a622
Kevin Charter <kevin.charter@acm.org>
2011-09-09 15:00:57 -0600

    Use Data.Map.Map Int instead of Data.IntMap.IntMap in Erl.EntityMap.
    This is the first of three steps to remove Data.IntMap from the
    implementation of Erl.EntityMap, and it's similar in spirit to
    the second-last commit. Here I've replaced Data.IntMap.IntMap
    with Data.Map.Map Int. In the next commit, I'll switch to
    using the more direct Data.Map.Map EntityId.
    
    The third step will involve fixing two deprecating warnings for
    Data.Map.fold, which is now replaced with Data.Map.foldr.
    
    At this point, everything builds and the unit tests pass.

--- Erl/EntityMap.hs
+++ Erl/EntityMap.hs
@@ -27,99 +27,99 @@ module Erl.EntityMap (EntityMap,
                      ) where
 
 import Control.Arrow (first)
-import qualified Data.IntMap as DIM
+import qualified Data.Map as DM
 import Prelude hiding (lookup, map, filter)
 import qualified Prelude as P
 
 import Erl.Entity (EntityId, toInt, fromInt)
 
-newtype EntityMap a = EntityMap (DIM.IntMap a) deriving (Eq, Ord, Show)
+newtype EntityMap a = EntityMap (DM.Map Int a) deriving (Eq, Ord, Show)
 
 empty :: EntityMap a
-empty = fromIntMap DIM.empty
+empty = fromIntMap DM.empty
 
 singleton :: EntityId -> a -> EntityMap a
-singleton id v = fromIntMap $ DIM.singleton (toInt id) v
+singleton id v = fromIntMap $ DM.singleton (toInt id) v
 
 lookup :: EntityId -> EntityMap a -> Maybe a
-lookup id m = DIM.lookup (toInt id) (toIntMap m)
+lookup id m = DM.lookup (toInt id) (toIntMap m)
 
 member :: EntityId -> EntityMap a -> Bool
-member id m = DIM.member (toInt id) (toIntMap m)
+member id m = DM.member (toInt id) (toIntMap m)
 
 contains :: EntityMap a -> EntityId -> Bool
 contains = flip member
 
 insert :: EntityId -> a -> EntityMap a -> EntityMap a
-insert i v = lift1 $ DIM.insert (toInt i) v
+insert i v = lift1 $ DM.insert (toInt i) v
 
 delete :: EntityId -> EntityMap a -> EntityMap a
-delete = lift1 . DIM.delete . toInt
+delete = lift1 . DM.delete . toInt
 
 ids :: EntityMap a -> [EntityId]
-ids = P.map fromInt . DIM.keys . toIntMap
+ids = P.map fromInt . DM.keys . toIntMap
 
 values :: EntityMap a -> [a]
-values = DIM.elems . toIntMap
+values = DM.elems . toIntMap
 
 toList :: EntityMap a -> [(EntityId, a)]
-toList = P.map (first fromInt) . DIM.toList . toIntMap
+toList = P.map (first fromInt) . DM.toList . toIntMap
 
 fromList :: [(EntityId, a)] -> EntityMap a
-fromList = fromIntMap . DIM.fromList . P.map (first toInt)
+fromList = fromIntMap . DM.fromList . P.map (first toInt)
 
-toIntMap :: EntityMap a -> DIM.IntMap a
+toIntMap :: EntityMap a -> DM.Map Int a
 toIntMap (EntityMap m) = m
 
-fromIntMap :: DIM.IntMap a -> EntityMap a
+fromIntMap :: DM.Map Int a -> EntityMap a
 fromIntMap = EntityMap
 
 union :: EntityMap a -> EntityMap a -> EntityMap a
-union = lift2 DIM.union
+union = lift2 DM.union
 
 unionWith :: (a -> a -> a) -> EntityMap a -> EntityMap a -> EntityMap a
-unionWith f = lift2 (DIM.unionWith f)
+unionWith f = lift2 (DM.unionWith f)
 
 difference :: EntityMap a -> EntityMap a -> EntityMap a
-difference = lift2 DIM.difference
+difference = lift2 DM.difference
 
 differenceWith :: (a -> a -> Maybe a) -> EntityMap a -> EntityMap a -> EntityMap a
-differenceWith f = lift2 (DIM.differenceWith f)
+differenceWith f = lift2 (DM.differenceWith f)
 
 intersection :: EntityMap a -> EntityMap a -> EntityMap a
-intersection = lift2 DIM.intersection
+intersection = lift2 DM.intersection
 
 intersectionWith :: (a -> a -> a) -> EntityMap a -> EntityMap a -> EntityMap a
-intersectionWith f = lift2 (DIM.intersectionWith f)
+intersectionWith f = lift2 (DM.intersectionWith f)
 
 fold :: (a -> b -> b) -> b -> EntityMap a -> b
-fold f i = DIM.fold f i . toIntMap
+fold f i = DM.fold f i . toIntMap
 
 foldWithKey :: (EntityId -> a -> b -> b) -> b -> EntityMap a -> b
-foldWithKey f i = DIM.foldWithKey f' i . toIntMap
+foldWithKey f i = DM.foldWithKey f' i . toIntMap
   where f' raw = f (fromInt raw)
 
 adjust :: (a -> a) -> EntityId -> EntityMap a -> EntityMap a
-adjust f i = lift1 $ DIM.adjust f (toInt i)
+adjust f i = lift1 $ DM.adjust f (toInt i)
 
 alter :: (Maybe a -> Maybe a) -> EntityId -> EntityMap a -> EntityMap a
-alter f i = lift1 $ DIM.alter f (toInt i)
+alter f i = lift1 $ DM.alter f (toInt i)
 
 map :: (a -> b) -> EntityMap a -> EntityMap b
-map = lift1 . DIM.map
+map = lift1 . DM.map
 
 mapMaybe :: (a -> Maybe b) -> EntityMap a -> EntityMap b
-mapMaybe = lift1 . DIM.mapMaybe
+mapMaybe = lift1 . DM.mapMaybe
 
 filter :: (a -> Bool) -> EntityMap a -> EntityMap a
-filter = lift1 . DIM.filter
+filter = lift1 . DM.filter
 
 filterWithKey :: (EntityId -> a -> Bool) -> EntityMap a -> EntityMap a
-filterWithKey pred = lift1 (DIM.filterWithKey pred')
+filterWithKey pred = lift1 (DM.filterWithKey pred')
   where pred' raw = pred (fromInt raw)
 
-lift1 :: (DIM.IntMap a -> DIM.IntMap b) -> EntityMap a -> EntityMap b
+lift1 :: (DM.Map Int a -> DM.Map Int b) -> EntityMap a -> EntityMap b
 lift1 f = fromIntMap . f . toIntMap
 
-lift2 :: (DIM.IntMap a -> DIM.IntMap a -> DIM.IntMap a) -> EntityMap a -> EntityMap a -> EntityMap a
+lift2 :: (DM.Map Int a -> DM.Map Int a -> DM.Map Int a) -> EntityMap a -> EntityMap a -> EntityMap a
 lift2 f em1 em2 = fromIntMap $ f (toIntMap em1) (toIntMap em2)
