def942aff2806e0ea0d51ee6799a6403e3f59bd4
Kevin Charter <kevin.charter@acm.org>
2011-09-09 14:52:09 -0600

    Use Data.Set.Set EntityId instead of Data.Set.Set Int in Erl.EntitySet.
    This completes the reversion to Data.Set.Set EntityId for EntitySets.
    At this point, we no longer use the 'toInt' and 'fromInt' functions
    on EntityIds in this module.
    
    Next, I'll have to make similar changes in Erl.EntityMap. Once that's
    done, we'll be able to remove the 'toInt' and 'fromInt' functions on
    EntityId, and be free to change its representation.

--- Erl/EntitySet.hs
+++ Erl/EntitySet.hs
@@ -20,22 +20,22 @@ import qualified Data.Set as DS
 
 import Erl.Entity
 
-newtype EntitySet = EntitySet (DS.Set Int) deriving (Eq, Ord, Show)
+newtype EntitySet = EntitySet (DS.Set EntityId) deriving (Eq, Ord, Show)
 
 empty :: EntitySet
 empty = EntitySet DS.empty
 
 singleton :: EntityId -> EntitySet
-singleton = fromSet . DS.singleton . toInt
+singleton = fromSet . DS.singleton
 
 insert :: EntityId -> EntitySet -> EntitySet
-insert id es = fromSet $ DS.insert (toInt id) (toSet es)
+insert id es = fromSet $ DS.insert id (toSet es)
 
 delete :: EntityId -> EntitySet -> EntitySet
-delete id es = fromSet $ DS.delete (toInt id) (toSet es)
+delete id es = fromSet $ DS.delete id (toSet es)
 
 member :: EntityId -> EntitySet -> Bool
-member id es = DS.member (toInt id) (toSet es)
+member id es = DS.member id (toSet es)
 
 contains :: EntitySet -> EntityId -> Bool
 contains = flip member
@@ -55,20 +55,20 @@ difference = lift2 DS.difference
 intersection :: EntitySet -> EntitySet -> EntitySet
 intersection = lift2 DS.intersection
 
-lift2 :: (DS.Set Int -> DS.Set Int -> DS.Set Int) -> EntitySet -> EntitySet -> EntitySet
+lift2 :: (DS.Set EntityId -> DS.Set EntityId -> DS.Set EntityId) -> EntitySet -> EntitySet -> EntitySet
 lift2 f es1 es2 = fromSet $ f (toSet es1) (toSet es2)
 
 toList :: EntitySet -> [EntityId]
-toList es = map fromInt $ DS.toList $ toSet es
+toList es = DS.toList $ toSet es
 
 fromList :: [EntityId] -> EntitySet
-fromList = fromSet . DS.fromList . map toInt
+fromList = fromSet . DS.fromList
 
 isSubsetOf :: EntitySet -> EntitySet -> Bool
 isSubsetOf es1 es2 = DS.isSubsetOf (toSet es1) (toSet es2)
 
-toSet :: EntitySet -> DS.Set Int
+toSet :: EntitySet -> DS.Set EntityId
 toSet (EntitySet s) = s
 
-fromSet :: DS.Set Int -> EntitySet
+fromSet :: DS.Set EntityId -> EntitySet
 fromSet = EntitySet
